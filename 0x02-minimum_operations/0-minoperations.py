b1fff4893f34e345010cc6902fc8571683503df1
Johnharry88 <harryjohnwhye@gmail.com>
2024-07-09 10:04:15 +0100

    Code challenge

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Minimum operation coding challenge"""
+
+
+def minOperations(n):
+    """Computes the fewest no of operaions
+    in exactly in H characters.
+    """
+    if not isinstance(n, int):
+        return 0
+    count_ops = 0
+    clipper = 0
+    complete = 1
+    
+    while complete < n:
+        if clipper == 0:
+            clipper = complete
+            complete = complete + clipper
+            count_ops = count_ops + 2
+
+        elif n - complete > 0 and (n - complete) % complete == 0:
+            clipper = complete
+            complete = complete + clipper
+            count_ops = count_ops + 2
+
+        elif clipper > 0:
+            complete = complete + clipper
+            count_ops = count_ops + 1
+    return count_ops
