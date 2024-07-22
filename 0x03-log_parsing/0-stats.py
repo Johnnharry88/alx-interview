ed2266c9a1624b8009bc5d08ab21b90f5e1e0960
Johnharry88 <harryjohnwhye@gmail.com>
2024-07-22 19:23:55 +0100

    Log Parse

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,47 +2,42 @@
 """ A module that reads stdin line by line and computes metrics"""
 
 import sys
+if __name__ == "__main__":
+    # holds count of all status code in a dictionary
+    status_code = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
+                   '404': 0, '405': 0, '500': 0}
+    # status_code iterator
+    stat = {k: 0 for k in status_code}
+    # FIles Size tracker
+    size_t = 0
+    # keeps record of the number of line
+    rec = 0
 
-# holds count of all status code in a dictionary
-status_code = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
-               '404': 0, '405': 0, '500': 0}
-
-size_t = 0
-# keeps record of the number of line
-rec = 0
-try:
-    for xl in sys.stdin:
-        first_line = lx.split(" ")
-        if len(first_line) > 4:
-            status_c = first_line[-2]
-            file_s = int(first_line[-1])
-
-            # check for status code in dict status_code and inrements
-            # its count if present
-
-            if status_c in status_code.keys():
-                status_code[status_c] += 1
-            # update total size
-            size_t = size_t + file_s
-
-            # kepps track of count
+    # function to print
+    def print_out(stat: dict, size_t: int) -> None:
+        print('File size: {:d}'.format(size_t))
+        for x, y in sorted(stat.items()):
+            if y != 0:
+                print("{}: {}".format(x, y))
+    try:
+        for xl in sys.stdin:
+            first_line = xl.split()
             rec = rec + 1
-
-        # reset count track to 0 if it gets to 10
-        if rec == 10:
-            rec = 0
-            print(f'^CFile size: {size_t}')
-
-            # prints outs status code count
-            for x, y in sorted(status_code.items()):
-                if y != 0:
-                    print('{}: {}'. format(x, y))
-
-except Exception as err:
-        pass
-
-finally:
-    print(f'^CFile size: {size_t}')
-    for x, y in sorted(status_code.items()):
-        if y != 0:
-            print('{}: {}'.format(x, y))
+            try:
+                status_c = first_line[-2]
+                if status_c in stat:
+                    stat[status_c] += 1
+            except BaseException:
+                pass
+            try:
+                file_x = int(first_line[-1])
+                size_t = size_t + file_x
+            except BaseException:
+                pass
+            if rec == 10:
+                rec = 0
+                print_out(stat, size_t)
+        print_out(stat, size_t)
+    except KeyboardInterrupt:
+        print_out(stat, size_t)
+        raise
