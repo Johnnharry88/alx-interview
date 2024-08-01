b2bc0aba36af756b7fa30c18efbee6f4558e04a6
Johnharry88 <harryjohnwhye@gmail.com>
2024-08-01 20:49:17 +0100

    Nqueen

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,83 +1,59 @@
 #!/usr/bin/python3
-"""Module that finds solutions for nqueen"""
+"""Module that finds solutio for nqueen"""
 import sys
 
-# list of possible solutions
-sol = []
-# size of cheese board n
-n = 0
-# possibe positions of quenn on chessboard
-pos = None
+
+def print_board(board, n):
+    """Print allocated positions to the queen"""
+    b = []
+
+    for i in range(n):
+        for j in range(n):
+            if j == board[i]:
+                b.append([i, j])
+    print(b)
 
 
-def input_getter():
-    """Checks program inputs and validates ;t """
-    global n 
-    n  = 0
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-    else:
-        try:
-            n = int(sys.argv[1])
-        except Exception:
-            print("N must be a numbre")
-            sys.exit(1)
-        if n < 4:
-            print("N must be at least 4")
-            sys.exit(1)
-        return n
+def is_position_safe(board, i, j, r):
+    """Checks if the position is safe for the queen"""
+    return board[i] in (j, j - i + r, i - r + j)
 
 
-def attacking_queen(pos0, pos1):
-    """Checks for that attacking mode of two queens"""
-    if (pos0[0] == pos1[0]) or (pos0[1] == pos1[1]):
-        return True
-    return abs(pos0[0] - pos1[0]) == abs(pos0[1] - pos1[1])
-
-
-def existing_grp(group):
-    """Checks out a group in the solution list"""
-    global sol
-    for s in sol:
-        x = 0
-        for s_pos in s:
-            for g_pos in group:
-                if s_pos[0] == g_pos[0] and s_pos[1] == g_pos[1]:
-                    x += 1
-        if x == n:
-            return True
-    return False
-
-def solver(row, group):
-    """Solution for hte nqueens problem"""
-    global sol
-    global n
+def safe_positions(board, row, n):
+    """Find all safe positions where the queen can be allocated"""
     if row == n:
-        top0 = group.copy()
-        if not existing_grp(top0):
-            sol.append(top0)
+        print_board(board, n)
+
     else:
-        for c in range(n):
-            x = (row * n) + c
-            check = zip(list([pos[x]]) * len(group), group)
-            used_pos = map(lambda n: attacking_queen(n[0], n[1]), check)
-            group.append(pos[x].copy())
-            if not any(used_pos):
-                solver(row + 1, group)
-            group.pop(len(group) - 1)
+        for j in range(n):
+            allowed = True
+            for i in range(row):
+                if is_position_safe(board, i, j, row):
+                    allowed = False
+            if allowed:
+                board[row] = j
+                safe_positions(board, row + 1, n)
 
 
-def get_sol():
-    """Retrieves solution for given chessboard size"""
-    global pos, n
-    pos = list(map(lambda x: [x //n, x% n], range(n ** 2)))
-    a = 0
-    group = []
-    solver(a, group)
+def create_board(size):
+    """Generates the board"""
+    return [0 * size for i in range(size)]
 
 
-n = input_getter()
-get_sol()
-for s in sol:
-    print(sol)
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    exit(1)
+
+try:
+    n = int(sys.argv[1])
+except BaseException:
+    print("N must be a number")
+    exit(1)
+
+if (n < 4):
+    print("N must be at least 4")
+    exit(1)
+
+board = create_board(int(n))
+row = 0
+safe_positions(board, row, int(n))
