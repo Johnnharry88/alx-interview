8e71156070ca64e4456c305c95d42ec9be071e65
Johnharry88 <harryjohnwhye@gmail.com>
2024-09-27 22:58:48 +0100

    Prime ame

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
 """Module for solving  primegame"""
 
+
 def isWinner(x, nums):
-     """Function to check fr the winner """
-     if not nums or x < 1:
-          return None
-     max_no = (max(nums))
-     selector = [True for y in range(max(max_no + 1, 2))]
-     for i in range(2, int(pow(max_no, 0.5)) + 1):
-          if not selector[i]:
-               continue
-          for j in range(i * i, max_no + 1, i):
-               selector[j] =  False
-     selector[0] = selector[1] = False
-     a = 0
-     for i in range(len(selector)):
-          if selector[i]:
-               a = a + 1;
-          selector[i] = a
-     gamer = 0
-     for x in nums:
-          gamer = gamer + selector[x] % 2 == 1
-     if gamer * 2 > len(nums):
-          return "Maria"
-     return "Ben"
+    """Function to check fr the winner """
+    if not nums or x < 1:
+        return None
+    max_no = (max(nums))
+    selector = [True for y in range(max(max_no + 1, 2))]
+    for i in range(2, int(pow(max_no, 0.5)) + 1):
+        if not selector[i]:
+            continue
+        for j in range(i * i, max_no + 1, i):
+            selector[j] = False
+    selector[0] = selector[1] = False
+    a = 0
+    for i in range(len(selector)):
+        if selector[i]:
+            a = a + 1
+        selector[i] = a
+    gamer = 0
+    for x in nums:
+        gamer = gamer + selector[x] % 2 == 1
+    if gamer * 2 > len(nums):
+        return "Maria"
+    return "Ben"
